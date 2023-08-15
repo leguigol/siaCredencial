0422c3fddc785e750752255715bfec7d960cba58
Legui <leguigol@hotmail.com>
2023-08-15 17:00:50 -0300

    1508

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,39 @@
 <template>
     <div>
         <h1>Home</h1>
-        <p>{{  userStore.userData?.email }}</p>
+        <p>{{ userStore.userData?.email }}</p>
+
+        <form @submit.prevent="handleSubmit">
+            <input type="text" placeholder="Ingrese URL" v-model="url">
+            <button type="submit">Agregar</button>
+        </form>
+        <p v-if="databaseStore.loadingDoc">Loading docs...</p>
+        <ul v-else>
+            <li v-for="item of databaseStore.documents" :key="item.id">
+                {{ item.id }} 
+                <br>
+                {{ item.name }}
+                <br>
+                {{ item.short }}
+            </li>
+        </ul>
     </div>
 </template>
 
 <script setup>
 import {useUserStore} from '../stores/user';
+import {useDatabaseStore} from '../stores/database';
+import { ref } from 'vue';
 
 const userStore=useUserStore();
+const databaseStore=useDatabaseStore();
+
+databaseStore.getUrls()
+
+const url=ref('');
+
+const handleSubmit= () => {
+    console.log('submit')
+}
 </script>
 
