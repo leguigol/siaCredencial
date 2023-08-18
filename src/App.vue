211d297be47713a03923984aa4015b6ecbcbfec7
legui <leguigol@hotmail.com>
2023-08-18 18:21:14 -0300

    ultima de wilde

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <template>
-  <a-layout style="background-color: white;">
-  <a-layout-header style="background-color: white; text-align: center; justify-content: center; height: 100px;">
-    <h1>SIA CLUB</h1>
-  </a-layout-header>
-    <!-- <nav v-if="!userStore.loadinSession">
+  <div>
+    <h1>Credencial</h1>
+    <nav v-if="!userStore.loadinSession">
       <router-link to="/" v-if="userStore.userData">Home</router-link> |
       <router-link to="/login" v-if="!userStore.userData">Login</router-link> |
       <router-link to="/Register" v-if="!userStore.userData">Register</router-link> |
@@ -11,29 +9,15 @@
     </nav>
     <div v-else>
       loading user....
-    </div> -->
-    <a-layout-content style="padding: 0 50px">
-      <div :style="{background: '#fff', padding: '24px', minHeight: '280px'} ">
-          <router-view></router-view>
-      </div>
-    </a-layout-content>
-    <a-layout-footer style="text-align: center;">
-      SIA CLUB 2023 Creado Por Leguicode
-    </a-layout-footer>
-  </a-layout>
-  
+    </div>
+    <router-view></router-view>
+  </div>
 </template>
 
 <script setup>
-import { onMounted } from 'vue';
-import Login from './views/Login.vue';
+import {useUserStore} from './stores/user';
 
-// import {useUserStore} from './stores/user';
-
-// const userStore=useUserStore();
-onMounted( async() => {
-
-});
+const userStore=useUserStore();
 
 </script>
 
